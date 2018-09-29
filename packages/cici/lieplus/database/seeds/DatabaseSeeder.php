a1e83cdb65ae995655b3c1c11763c27fbfb1f4f2
Aleutian Xie <aleutian.xie@cicisoft.cn>
2018-09-30 00:15:50 +0800

    create roles and admin user

--- packages/cici/lieplus/database/seeds/DatabaseSeeder.php
+++ packages/cici/lieplus/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        exit;
         $this->call(RegionsTableSeeder::class);
 //        $this->call(UsersTableSeeder::class);
 //        $this->call(RolesTableSeeder::class);
--- packages/cici/lieplus/database/seeds/UsersTableSeeder.php
+++ packages/cici/lieplus/database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,16 +13,35 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $this->command->comment('插入全国省市县信息...');
-        $this->command->info('插入全国省市县信息...');
-        $this->command->question('插入全国省市县信息...');
-        $this->command->error('插入全国省市县信息...');
-        exit;
-        DB::table('users')->insert([
-            'name' => 'Aleutian Xie',
-            'email' => 'aleutian.xie@cicisoft.cn',
-            'password' => '$2y$10$MZiQ5tKtTSxY0J.oIXXfZe38oEpqrJtLnES5lTnyZ2fagcozIYZiu',
-            'created_at' => date('Y-m-d H:i:s', time()),
-            'updated_at' => date('Y-m-d H:i:s', time())]);
+//        $this->command->comment('插入全国省市县信息...');
+//        $this->command->info('插入全国省市县信息...');
+//        $this->command->question('插入全国省市县信息...');
+//        $this->command->error('插入全国省市县信息...');
+
+        $this->command->question('创建一个管理员用户...');
+        $name = $this->command->ask('用户名:', "Aleutian Xie");
+        $email = $this->command->ask('邮箱:', "aleutian.xie@cicisoft.cn");
+        $password = $this->command->secret("密码:");
+        $password = password_hash($password,PASSWORD_BCRYPT);
+        $created_at = date('Y-m-d H:i:s', time());
+        $updated_at = date('Y-m-d H:i:s', time());
+        DB::table('users')->insert(compact('name', 'email', 'password', 'created_at', 'updated_at'));
+        $this->command->info('创建管理员完成!');
+
+        $this->command->comment('创建角色和分配管理员角色...');
+        $this->command->comment("Create Role admin.");
+        Role::create(['name' => 'admin']);
+        $this->command->comment("Create Role manager.");
+        Role::create(['name' => 'manager']);
+        $this->command->comment("Create Role bd.");
+        Role::create(['name' => 'bd']);
+        $this->command->comment("Create Role recruiter.");
+        Role::create(['name' => 'recruiter']);
+        $this->command->comment("Create Role customer.");
+        Role::create(['name' => 'customer']);
+        $this->command->comment("Assign Role admin to $name.");
+        $user = User::where(compact('name'))->first();
+        $user->assignRole('admin');
+        $this->command->info('创建角色和分配管理员角色完成!');
     }
 }
